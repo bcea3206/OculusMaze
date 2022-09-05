0a2821f8b56c5e37bf2a555d13bb6412ba781903
bcea3206 <80414706+bcea3206@users.noreply.github.com>
2022-09-05 19:16:34 -0400

    Initial commit

---
+++ Assets/CheckWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject winMessage;
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+
+            if (!winMessage.activeInHierarchy)
+                winMessage.SetActive(true);
+        }
+    }
+}
+
